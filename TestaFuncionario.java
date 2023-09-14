af633c81c5ffc7b1bc34ec0de8909d42a4a3d0da
Eduardo Nogueira <94703120+aboras7@users.noreply.github.com>
2023-09-14 12:56:26 -0300

    Add files via upload
    Adicionado o try/catch

--- TestaFuncionario.java
+++ TestaFuncionario.java
@@ -1,18 +1,47 @@
-package poo;
+package interfaces;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TestaFuncionario {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
 
-        Vendedor funcionario1 = new Vendedor("Carla", 'F', 26, "Matutino", 14567, true);
-        funcionario1.visualizar();
+        System.out.println("Digite o nome do funcionário ");
+        String nomeFun = leia.nextLine();
 
-        Vendedor funcionario2 = new Vendedor("Júlio", 'M', 32, "Vespertino", 15649, true);
-        funcionario2.visualizar();
+        System.out.println("Digite a idade: ");
+        int idadeFun = Integer.parseInt(leia.nextLine());
 
-        Gerente funcionario3 = new Gerente("Roberto", 'M', 45, "Horário Admnistrativo", 16478, true, true);
-        funcionario3.visualizar();
+        Gerente gerente1 = new Gerente(nomeFun, idadeFun);
 
-        Gerente funcionario4 = new Gerente("Larissa", 'F', 34, "Noturno", 16498, true, true);
-        funcionario4.visualizar();
+        System.out.println("Atributos de um Gerente: ");
+        System.out.println(gerente1.getNome() + "," + gerente1.getIdade());
+        gerente1.abrirLoja();
+        gerente1.vender();
+        gerente1.almocar();
+        gerente1.organizarCabides();
+        gerente1.fecharLoja();
+
+        Vendedor vendedor1 = new Vendedor("Júlio", 33);
+
+        System.out.println("Atributos de um Vendedor: ");
+        vendedor1.abrirLoja();
+        vendedor1.vender();
+        vendedor1.almocar();
+        vendedor1.organizarCabides();
+
+        try {
+            int valorCaixa;
+            System.out.println("Digite o valor final do caixa: ");
+            valorCaixa = leia.nextInt();
+            System.out.println("Valor final do caixa: " + valorCaixa);
+
+        } catch (InputMismatchException e) {
+            System.err.println("\nExceção: " + e);
+            System.out.println("Digite um número inteiro: ");
+        }
+        vendedor1.fecharLoja();
     }
 }
+
